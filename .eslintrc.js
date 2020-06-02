1547208270d8f01e8a275f7445776234332a3714
Mateus Vieira <mateus17v@gmail.com>
2020-06-02 18:50:11 -0300

    Update - Config Eslint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,9 @@ module.exports = {
     node: true,
   },
   extends: [
+    'prettier',
     'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended'
   ],
   globals: {
     Atomics: 'readonly',
@@ -12,28 +14,39 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
+    'project': './tsconfig.json',
+    ecmaVersion: 2020,
     sourceType: 'module',
+    'ecmaFeatures': {
+      'jsx': true
+    },
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
   },
   plugins: [
     '@typescript-eslint',
+    "eslint-plugin-prettier"
   ],
   rules: {
     'prettier/prettier': 'error',
-  'react/jsx-filename-extension': [
-    'warn',
-    {
-      extensions: ['.jsx', '.js', '.ts', '.tsx']
-    }
-  ],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js', '.ts', '.tsx']
+      }
+    ],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': 'error',
     'import/prefer-default-export': 'off',
-    "react/state-in-constructor": 'off',
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "import/no-unresolved": "off",
+    'react/state-in-constructor': 'off',
+    'import/no-unresolved': 'off',
   },
 };
