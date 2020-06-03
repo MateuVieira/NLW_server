5e3655ebd35f16344bdbd439271083e18c0ad58d
Mateus Vieira <mateus17v@gmail.com>
2020-06-02 21:51:45 -0300

    Update routes - add list all items

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import kenx from './database/connection';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('API ON');
+});
+
+routes.get('/items', async (req, res) => {
+  const items = await kenx('items').select('*');
+
+  const serializedItems = items.map((item) => ({
+    title: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return res.json(serializedItems);
 });
 
 export default routes;
